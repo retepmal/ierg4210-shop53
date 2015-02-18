53f060d9d505328716d1a31c952ecba102aa1db6
Peter Lam <peter@retepmal.com>
2015-02-18 16:53:05 +0800

    Simplify RDS credential in environment variable

--- shop53.config.js
+++ shop53.config.js
@@ -1,8 +1,3 @@
-var dbhost = process.env.RDS_HOSTNAME,
-    dbuser = process.env.RDS_USERNAME,
-    dbpass = process.env.RDS_PASSWORD,
-    dbname = process.env.RDS_DATABASE;
-
 module.exports = {
-    "dbURI": "mysql://" + dbuser + ":" + dbuser + "@" + dbhost + "/" + dbname
+    "dbURI": process.env.RDS_CREDENTIAL
 };
